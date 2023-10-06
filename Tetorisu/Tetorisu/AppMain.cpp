338dd9fda71c4cd86ded1495b455b7318a3c24cf
新崎雄大 <138739177+YuutaArasaki@users.noreply.github.com>
2023-10-06 11:27:43 +0900

    テトリス制作
    Appmainにプログラムを書き込んだ

--- Tetorisu/Tetorisu/AppMain.cpp
+++ Tetorisu/Tetorisu/AppMain.cpp
@@ -1,6 +1,41 @@
-#include"DxLib.h" 
+#include"DxLib.h"
+#include"FreamControl.h"
+#include"SceneManager.h"
+#include"InputControl.h"
+
+
+/******************************************
+*マクロ定義
+******************************************/
+
+#define SCREEN_HEIGHT (720)
+#define SCREEN_WIDTH  (1280)
+#define SCREEN_COLORBIT (32)
+#define FONT_SIZE (20)
+
+/*****************************************
+*型定義
+******************************************/
+
+
+/*****************************************
+*グローバル変数定義
+******************************************/
+
+/*****************************************
+*プロトタイプ宣言
+******************************************/
+
+
+/*****************************************
+*プログラムの開始
+******************************************/
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
+	//タイトルを変更
+	SetMainWindowText("テトリス");
+
 	//ウィンドウモードで起動
 
 	ChangeWindowMode(TRUE);
