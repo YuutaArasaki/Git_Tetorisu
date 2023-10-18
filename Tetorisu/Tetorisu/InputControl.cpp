5cc9c098fc57600518ee37277211ed3f9c57c8f4
新崎雄大 <138739177+YuutaArasaki@users.noreply.github.com>
2023-10-18 15:01:48 +0900

    テトリス制作
    cppファイルにプログラムを書き込んだ

--- Tetorisu/Tetorisu/InputControl.cpp
+++ Tetorisu/Tetorisu/InputControl.cpp
@@ -103,4 +103,124 @@ void InputoControl_Update(void)
 		}
 	}
 }
-//３ページ目
+
+
+/***********************************************
+* 入力制御機能：押されているか判定処理
+* 引数：XINPUTのボタン情報
+* XINPUT_BUTTON_DPAD＿UP			(0)		//デジタル方向ボタン上
+* XINPUT_BUTTON_DPAD_DOWN		(1)		//デジタル方向ボタン下
+* XINPUT_BUTTON_DPAD_LEFT		(2)		//デジタル方向ボタン左
+* XINPUT_BUTTON_DPAD_RIGHT		(3)		//デジタル方向ボタン右
+* XINPUT_BUTTON_STATE			(4)		//STARTボタン
+* XINPUT_BUTTON_BACK			(5)		//BACKボタン
+* XINPUT_BUTTON_LEFT＿THUMB		(6)		//左ステック押し込み
+* XINPUT_BUTTON_RIGHT＿THUMB	(7)		//右ステック押し込み
+* XINPUT_BUTTON＿LEFT_SHOULDER	(8)		//LBボタン
+* XINPUT_BUTTON＿RIGHT_SHOULDER	(9)		//RBボタン
+* XINPUT_BUTTON_A					(12)		//Aボタン
+* XINPUT_BUTTON_B					(12)		//Bボタン
+* XINPUT_BUTTON_X					(13)		//Xボタン
+* XINPUT_BUTTON_Y					(14)		//Yボタン
+* 戻り値：TRUE（押されてる）、FALSE（押されてない）
+*************************************************/
+
+int GetButton(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_CLICK || button_state[button] == E_PRESS)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+}
+
+
+/************************************************
+* 入力制御機能：押した瞬間か判定処理
+* 引数：XINPUTのボタン情報
+* XINPUT_BUTTON_DPAD＿UP			(0)			//デジタル方向ボタン上
+* XINPUT_BUTTON_DPAD＿DOWN		(1)			//デジタル方向ボタン下
+* XINPUT_BUTTON_DPAD＿LEFT		(2)			//デジタル方向ボタン左
+* XINPUT_BUTTON_DPAD＿RIGHT		(3)			//デジタル方向ボタン右
+* XINPUT_BUTTON_STATE			(4)			//STARTボタン
+* XINPUT_BUTTON_BACK			(5)			//BACKボタン
+* XINPUT_BUTTON_LEFT_THUMB		(6)			//左ステック押し込み
+* XINPUT_BUTTON_RIGHT_THUMB		(7)			//右ステック押し込み
+* XINPUT_BUTTON＿LEFT_SHOULDER	(8)		//LBボタン
+* XINPUT_BUTTON＿RIGHT_SHOULDER	(9)		//RBボタン
+* XINPUT_BUTTON_A					(12)		//Aボタン
+* XINPUT_BUTTON_B					(12)		//Bボタン
+* XINPUT_BUTTON_X					(13)		//Xボタン
+* XINPUT_BUTTON_Y					(14)		//Yボタン
+* 戻り値：TRUE（押した瞬間）、FALSE（押した瞬間ではない）
+*************************************************/
+
+int GetButtonDown(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_CLICK)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+}
+
+
+/************************************************
+* 入力制御機能：離したか判定処理
+* 引数：XINPUTのボタン情報
+* XINPUT_BUTTON_DPAD＿UP			(0)			//デジタル方向ボタン上
+* XINPUT_BUTTON_DPAD＿DOWN		(1)			//デジタル方向ボタン下
+* XINPUT_BUTTON_DPAD＿LEFT		(2)			//デジタル方向ボタン左
+* XINPUT_BUTTON_DPAD＿RIGHT		(3)			//デジタル方向ボタン右
+* XINPUT_BUTTON_STATE			(4)			//STARTボタン
+* XINPUT_BUTTON_BACK			(5)			//BACKボタン
+* XINPUT_BUTTON_LEFT_THUMB		(6)			//左ステック押し込み
+* XINPUT_BUTTON_RIGHT_THUMB		(7)			//右ステック押し込み
+* XINPUT_BUTTON＿LEFT_SHOULDER	(8)		//LBボタン
+* XINPUT_BUTTON＿RIGHT_SHOULDER	(9)		//RBボタン
+* XINPUT_BUTTON_A					(12)		//Aボタン
+* XINPUT_BUTTON_B					(12)		//Bボタン
+* XINPUT_BUTTON_X					(13)		//Xボタン
+* XINPUT_BUTTON_Y					(14)		//Yボタン
+* 戻り値：TRUE（押した瞬間）、FALSE（押した瞬間ではない）
+*************************************************/
+
+int GetbuttonUp(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_RELEASED)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+
+}
+
+
+/**********************************************
+*入力制御機能：ゲーム終了用のボタン判定処理
+* 引数：なし
+* 戻り値：TRUE（押した瞬間）、FALSE（押した瞬間ではない）
+***********************************************/
+
+int GetExitButton(void)
+{
+	int ret = FALSE;
+
+	if ((GetButtonDown(XINPUT_BUTTON_BACK) == TRUE) || (CheckHitKey(KEY_INPUT_ESCAPE) ==
+		TRUE))
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+
+}
